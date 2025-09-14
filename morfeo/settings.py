a3ac01a5380e4f4c8d30857798f00a1e19d6745a
pardobenjamin.r@gmail.com <pardobenjamin.r@gmail.com>
2025-09-14 18:48:38 -0300

    actualizacion settings

--- morfeo/settings.py
+++ morfeo/settings.py
@@ -41,9 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
-    'tailwind',
-    
+    'app',    
 ]
 
 MIDDLEWARE = [
