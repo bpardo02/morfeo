44d2399693ca9f88db0ef4752563e15e94e5f205
Benjamín Pardo <105931799+bpardo02@users.noreply.github.com>
2025-12-01 00:26:29 -0300

    Switch database configuration to use dj_database_url

--- morfeo/settings.py
+++ morfeo/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import os
+
 
 load_dotenv()
 
@@ -79,12 +81,12 @@ WSGI_APPLICATION = 'morfeo.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
